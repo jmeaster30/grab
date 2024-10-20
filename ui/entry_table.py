39e33087f3d01838cb6130e0cf092c46d5530aec
jmeaster30 <jhneasterday09@gmail.com>
2024-10-19 21:30:24 -0400

    Removed bad import

--- ui/entry_table.py
+++ ui/entry_table.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 from typing import Callable, Optional
 
-from ui.scrollable_frame import ScrollableFrame
 from ui.scrollable_widget_list import ScrollableWidgetList
 
 class EntryTable(tk.Frame):
